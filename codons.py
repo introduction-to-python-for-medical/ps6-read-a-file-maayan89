e424ffda7404e6cbb587a9986943eef785d0213c
maayan89 <maayan089@gmail.com>
2024-12-11 17:01:17 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,12 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    def create_codon_dict(file_path):
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
+    d = {}
+    for row in rows:
+      line = row.strip().split('\t')
+      d[line[0]] = line[2]
+    return d
 
 
